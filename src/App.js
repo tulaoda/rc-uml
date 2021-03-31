ab15282f7aa19daf831f2a9bae84873c881fb616
tulaoda <coderaxin@163.com>
2021-03-31 10:24:53 +0800

    feat: init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css'
+import { Cytoscape } from '@mlamp/cytoscape-react'
+const defaultElements = {
+    nodes: [
+        { group: 'nodes', data: { id: 'a', label: 'apple' }, position: { x: 0, y: 0 } },
+        { group: 'nodes', data: { id: 'b', label: 'banana' }, position: { x: 100, y: 0 } },
+        { group: 'nodes', data: { id: 'c', label: 'cherry' }, position: { x: 200, y: 0 } },
+    ],
 }
 
-export default App;
+const defaultProps = {
+    className: '',
+    global: '',
+    elements: defaultElements,
+    layout: {
+        name: 'random',
+    },
+    layoutSelected: true,
+    boxSelect: false,
+    stylesheet: [
+        {
+            selector: '.hexagon',
+            style: {
+                shape: 'polygon',
+                'shape-polygon-points':
+                    '0 1, 0.8660254037844386 0.5, 0.8660254037844386 -0.5, 0 -1, -0.8660254037844386 -0.5, -0.8660254037844386 0.5',
+            },
+        },
+    ],
+    // 事件回调
+    onDestroy: () => {},
+    afterInit: () => {},
+}
+
+function App() {
+    return <Cytoscape {...defaultProps} />
+}
+
+export default App
